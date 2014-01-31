c371f531a3617ca560cb636d9e3a51181ee0d83b
aslabde <aslabde@gmailcom>
2014-02-01 00:48:02 +0100

    Postgres integration OK. Properties read as enviroment OK

--- SpringJPA/src/main/java/tk/ebalsa/SpringJPA/MyApp.java
+++ SpringJPA/src/main/java/tk/ebalsa/SpringJPA/MyApp.java
@@ -1,19 +1,22 @@
 package tk.ebalsa.SpringJPA;
 
+import com.jolbox.bonecp.BoneCPDataSource;
+
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate3.HibernateExceptionTranslator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -22,19 +25,32 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "tk.ebalsa.SpringJPA",
 includeFilters = @ComponentScan.Filter(value = {BookRepository.class}, type = FilterType.ASSIGNABLE_TYPE))
 @EnableTransactionManagement
+@PropertySource("classpath:/app.properties")
 public class MyApp {
+	
+
+    @Autowired
+    private Environment environment;
+    
 
     @Bean
-    public DataSource dataSource() throws SQLException {
+    public DataSource dataSource() {
+        BoneCPDataSource dataSource = new BoneCPDataSource();
 
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.H2).build();
-      }
+        dataSource.setDriverClass(environment.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setJdbcUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+
+        return dataSource;
+    }
+
 
     @Bean
     public EntityManagerFactory entityManagerFactory() throws SQLException {
@@ -47,7 +63,7 @@ public class MyApp {
       factory.setPackagesToScan("tk.ebalsa.SpringJPA");
       factory.setDataSource(dataSource());
       factory.afterPropertiesSet();
-
+      
       return factory.getObject();
     }
 
@@ -72,7 +88,7 @@ public class MyApp {
     public static void main(String[] args) {
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(MyApp.class);
         BookRepository repository = context.getBean(BookRepository.class);
-
+        
         // save a couple of books
         repository.save(new Book("title1", "auth1"));
         repository.save(new Book("Bible", "anonymus"));
